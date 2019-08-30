0e8fdaad67a3bd67b2d2597c3074f82370083050
Gustavo Moura <gnunes.moura@gmail.com>
2019-08-29 21:36:00 -0300

    correções getting started

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
 const { ApolloServer, gql } = require('apollo-server');
 
-const getUser = token => (token === 'tokenDeAcesso' ? ({ id: 12345, roles: ['user', 'admin'] }) : null);
+const getUser = token => (token === 'tokenDeAcesso' ? ({ id: 12345, name: 'bobão', roles: ['user', 'admin'] }) : null);
 
 /**
  * O context é um objeto que é passado para todos os resolvers, útil para passar conexões com o
  * banco de dados, váriaveis de ambiente, usuario cadastrado, etc...
  */
 const server = new ApolloServer({
-  typeDefs: gql`
-  type Query {
-    user (id: ID!): User
-   }
-   
-   type User {
-    id: ID!
-    name: String!
-   }`,
   resolvers: {
-    users: (parent, args, context) => {
-      // to-do throw an error.
-      if (!context.user) return null;
+    Query: {
+      user: (parent, args, context) => {
+        // to-do throw an error.
+        if (!context.user) return null;
 
-      return ['bob', 'jake'];
+        return context.user;
+      },
     },
   },
+  typeDefs: gql`
+  type User {
+    id: ID!
+    name: String!
+  }
+  
+  type Query {
+    user (id: ID!): User
+  }
+  `,
   context: ({ req }) => {
     const token = req.headers.authorization || '';
 
@@ -34,7 +37,3 @@ const server = new ApolloServer({
 });
 
 module.exports = server;
-
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
